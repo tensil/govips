ab8da00ba9a108e48843b4093d5d97de9e606c1b
Ryan Cunningham <rcunning@gmail.com>
2020-03-23 14:41:45 -0700

    dont forget to unref

--- pkg/vips/transform.go
+++ pkg/vips/transform.go
@@ -428,6 +428,8 @@ func (t *Transform) ApplyMemory() (*ImageRef, error) {
 		return nil, err
 	}
 
+	defer C.g_object_unref(C.gpointer(transformed))
+
 	return NewImageRef(transformed, imageType), nil
 }
 
